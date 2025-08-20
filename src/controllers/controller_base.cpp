a720e014f1a4ba5e5491df1677d807b5583f348b
dgordenin <dgordenin@gmail.com>
2025-08-20 12:54:56 +0300

    Moving to AGPL.

--- src/controllers/controller_base.cpp
+++ src/controllers/controller_base.cpp
@@ -2,7 +2,7 @@
  * Yutovo Server
  * Copyright (C) 2022-2025 Yutovo developers. All rights reserved.
  * This file is a part of the Yutovo project
- * SPDX-License-Identifier: GPL-3.0-only
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 #include "controller_base.h"
